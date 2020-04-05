01d148f015754c79956041cf671ceb0a1bb0bbd4
JoseOgam <jotienoochieng@gmail.com>
2020-04-05 08:29:03 +0300

    initial commit, adminlte,Fontawsome configured

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" type="image/ico" href="{{('images/devs/icon.png')}}"/>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    <link href="{{('/css/app.css')}}" rel="stylesheet">
+
+</head>
+<body class="hold-transition sidebar-mini">
+<div id="app" class="wrapper">
+
+    <!-- Navbar -->
+    <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+        <!-- Left navbar links -->
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
+            </li>
+        </ul>
+
+        <!--searching tab-->
+        <div class="input-group input-group-sm">
+            <input class="form-control form-control-navbar"
+                   type="search" placeholder="Search" aria-label="Search">
+            <div class="input-group-append">
+                <button class="btn btn-navbar" type="submit">
+                    <i class="fa fa-search"></i>
+                </button>
+            </div>
+        </div>
+
+        <!-- Right navbar links -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Messages Dropdown Menu -->
+            <!-- Notifications Dropdown Menu -->
+
+            <li class="nav-item">
+                <a class="nav-link" data-widget="control-sidebar" data-slide="true" href="#"><i
+                        class="fa fa-th-large"></i></a>
+            </li>
+        </ul>
+    </nav>
+    <!-- /.navbar -->
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+        <!-- Brand Logo -->
+        <a href="#" class="brand-link">
+            <img src="{{('images/icon.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
+                 style="opacity: .8">
+            <span class="brand-text font-weight-light">Portfolio</span>
+        </a>
+
+        <!-- Sidebar -->
+        <div class="sidebar">
+            <!-- Sidebar user panel (optional) -->
+            <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+                <div class="image">
+                    <img src="{{('images/icon.png')}}" class="img-circle elevation-2" alt="User Image">
+                </div>
+                <div class="info">
+                    <a href="#" class="d-block"> {{ Auth::user()->name }}</a>
+                    <a href="#" class="d-block"> {{ Auth::user()->type }}</a>
+                </div>
+            </div>
+
+            <!-- Sidebar Menu -->
+            <nav class="mt-2">
+                <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+                    data-accordion="false">
+                    <!-- Add icons to the links using the .nav-icon class
+                         with font-awesome or any other icon font library -->
+                    <li class="nav-item">
+                        <a to="/dashboard" class="nav-link ">
+                            <i class="nav-icon fas fa-tachometer-alt blue"></i>
+                            <p>dashboard</p>
+                        </a>
+                    </li>
+
+                        <li class="nav-item has-treeview">
+                            <a href="#" class="nav-link">
+                                <i class="nav-icon fas fa-cog green"></i>
+                                <p>
+                                    Management
+                                    <i class="right fa fa-angle-left"></i>
+                                </p>
+                            </a>
+                            <ul class="nav nav-treeview">
+                                <li class="nav-item">
+                                    <a to="/users" class="nav-link ">
+                                        <i class="nav-icon fas fa-users violet"></i>
+                                        <p>users</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a to="work" class="nav-link ">
+                                        <i class="nav-icon fas fa-briefcase purple"></i>
+                                        <p>Add Bus</p>
+                                    </a>
+                                </li>
+                                <li class="nav-item">
+                                    <a to="/messages" class="nav-link ">
+                                        <i class="nav-icon fas fa-envelope indigo"></i>
+                                        <p>messages</p>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        <li class="nav-item">
+                            <a to="/developer" class="nav-link ">
+                                <i class="nav-icon fas fa-cogs gray"></i>
+                                <p>developer</p>
+                            </a>
+                        </li>
+
+                    <li class="nav-item">
+                        <a to="/profile" class="nav-link ">
+                            <i class="nav-icon fas fa-user-alt orange"></i>
+                            <p>profile</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}"
+                           onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                            <i class="nav-icon fas fa-power-off red"></i>
+                            <p>  {{ __('Logout') }}</p>
+                        </a>
+
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                              style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+
+                </ul>
+
+            </nav>
+            <!-- /.sidebar-menu -->
+        </div>
+        <!-- /.sidebar -->
+    </aside>
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <div class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                    </div><!-- /.col -->
+                </div><!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content-header -->
+
+        <!-- Main content -->
+        <div class="content">
+            <div class="container-fluid">
+
+
+
+            </div><!-- /.container-fluid -->
+        </div>
+        <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+        <div class="p-3">
+            <h5>Title</h5>
+            <p>Sidebar content</p>
+        </div>
+    </aside>
+    <!-- /.control-sidebar -->
+
+    <!-- Main Footer -->
+    <footer class="main-footer">
+        <!-- To the right -->
+        <div class="float-right d-none d-sm-inline">
+            Legit Design
+        </div>
+        <!-- Default to the left -->
+        <strong>Copyright &copy; legit</strong> All rights reserved.
+    </footer>
+</div>
+<!-- js -->
+
+@auth
+    <script>
+        window.user = @json(auth()->user());
+    </script>
+@endauth
+</body>
+</html>
